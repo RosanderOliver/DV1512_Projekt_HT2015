c81da5857f24528f02a77a5459e101c0d556444f
Annika <akinna_nossnah@hotmail.com>
2015-09-25 12:18:39 +0200

    La till indexfil

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+  <title>Startsida</title>
+</head>
+<body>
+
+<form action="#" method="post">
+  Username: <input type="text" name="username"></br>
+  Password: <input type="password" name="psw"></br>
+  <input type="submit">
+</form>
+
+
+</body>
+</html>
