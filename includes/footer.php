79e6aef351785365c85e2d4d82f0b3a3cb6753d8
Jim Ahlstrand <jiah13@student.bth.se>
2015-11-04 10:16:07 +0100

    Added footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,11 @@
-</body>
+
+    <div id="footer">
+      <?php echo date("F j, Y, g:i a"); ?> | &copy; <?php echo date("Y"); ?> Exam Manager
+    </div>
+  </body>
 </html>
 
 <?php
 
-// Close database connection 
+// Close database connection
 $dbh = null;
--- includes/header.php
+++ includes/header.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="keywords" content="easy, exam, management">
-    <meta name="description" content="Easy management of exam submission, reviewing and grading.">
-    <title>Easy Exam Manger</title>
+    <meta name="description" content="Management of exam submission, reviewing and grading have never been easier.">
+    <title>Exam Manger</title>
     <link rel="stylesheet" href="/includes/css/stylesheet.css">
 </head>
 <body>
