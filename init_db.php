43dc7f41edd6c76bf7de91c0ad231e5dbd5201c7
Jim Ahlstrand <ahlstrand.jim@gmail.com>
2015-11-16 16:22:52 +0100

    Added more tables in database init
    No complete yet, some more adjusting of the tables are needed

--- init_db.php
+++ init_db.php
@@ -3,6 +3,16 @@
   // Database file
   // Here we define functions to initialize database
 
+  // TODO
+  // Seperate data storage and data handling, tables to store data and metadata and tables to link that data together
+  // Move project stage to files?
+  // Create projects database to handle projects and let courses and files etc store data
+  // Move deadlines to files or stated above projects?
+  // Filetypes?
+  // Add searched for viruses bool in files
+  // Move file grading to projects or some other handler
+  // Move file written reviews to some handler
+
   // Set site variable
   define('IN_EXM', TRUE);
   //if (!defined(IN_PR)) exit;
@@ -20,15 +30,67 @@
       exit;
   }
 
-  // Create database groups
-  $dbh->exec("CREATE DATABASE IF NOT EXISTS `groups`;")  or die(print_r($dbh->errorInfo(), true));
+  // Create database site
+  $dbh->exec("CREATE DATABASE IF NOT EXISTS `site`;")  or die(print_r($dbh->errorInfo(), true));
 
-  // Create table for student group
-  $dbh->exec("CREATE TABLE IF NOT EXISTS `groups`.`student` (
-      `id`                    INT       NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing id',
-      `create_course`         TINYINT   NOT NULL DEFAULT 0 COMMENT 'can the user create new courses',
+  // Users
+  $dbh->exec("CREATE TABLE IF NOT EXISTS `site`.`users` (
+      `id`                    INT          NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing id, unique',
+      `eppn`                  VARCHAR(16)  NOT NULL COMMENT 'user''s identifier, unique',
+      `user_email`            VARCHAR(64)  NOT NULL COMMENT 'user''s email, unique',
+      `user_name`             VARCHAR(64)  NOT NULL COMMENT 'user''s real name',
+      `courses`               VARCHAR(32)  NOT NULL DEFAULT 'a:0:{}' COMMENT 'serialized array with user''s enlisted courses',
+      UNIQUE KEY `eppn` (`eppn`),
+      UNIQUE KEY `user_email` (`user_email`),
       PRIMARY KEY (`id`)
-   ) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='student permissions';") or die(print_r($dbh->errorInfo(), true));
+   ) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='user specific data like email and unique identifier';")
+   or die(print_r($dbh->errorInfo(), true));
+
+  // Courses
+  $dbh->exec("CREATE TABLE IF NOT EXISTS `site`.`courses` (
+      `id`                    INT          NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing id',
+      `role_table`            INT          NOT NULL COMMENT 'table of permissions id',
+      `deadlines`             VARCHAR(64)  NOT NULL COMMENT 'array with the course''s dates for dealines',
+      `files`                 VARCHAR(128) NOT NULL DEFAULT 'a:0:{}' COMMENT 'files assosiated with the course',
+      `project_stage`         INT          NOT NULL DEFAULT 0 COMMENT 'stage of the projects',
+      PRIMARY KEY (`id`)
+   ) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='course roles, files and other data related to each course';")
+   or die(print_r($dbh->errorInfo(), true));
+
+  // Files
+  $dbh->exec("CREATE TABLE IF NOT EXISTS `site`.`files` (
+      `id`                    INT          NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing id',
+      `type`                  VARCHAR(64)  NOT NULL COMMENT 'Filetype',
+      `upload_user`           INT          NOT NULL COMMENT 'user id of the uploader',
+      `upload_date`           DATETIME     NOT NULL DEFAULT NOW() COMMENT 'date and time of the upload',
+      `path`                  VARCHAR(64)  NOT NULL COMMENT 'relative path to the file',
+      `comment_id`            INT          NOT NULL DEFAULT -1 COMMENT 'id of comment related to the file, -1 if none',
+      `reviews_id`            VARCHAR(64)  NOT NULL DEFAULT 'a:0:{}' COMMENT 'array with written reviews for the file',
+      `reviewers_id`          VARCHAR(64)  NOT NULL DEFAULT 'a:0:{}' COMMENT 'array with user id of reviewers',
+      `grade`                 INT          NOT NULL DEFAULT 0 COMMENT 'grade of the file',
+      PRIMARY KEY (`id`)
+   ) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='data about all uploaded files';") or die(print_r($dbh->errorInfo(), true));
+
+  // Comments id INT, comment_text TEXT,date DATETIME, user INT, responses INT
+  $dbh->exec("CREATE TABLE IF NOT EXISTS `site`.`comments` (
+      `id`                    INT       NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing id',
+      PRIMARY KEY (`id`)
+   ) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='';") or die(print_r($dbh->errorInfo(), true));
+
+  // Reviews id INT,data TEXT, uploader INT, upload_date DATETIME, last_modified TEXT,form_info BLOB
+  $dbh->exec("CREATE TABLE IF NOT EXISTS `site`.`reviews` (
+      `id`                    INT       NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing id',
+      PRIMARY KEY (`id`)
+   ) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='';") or die(print_r($dbh->errorInfo(), true));
+
+  // Create database permissions
+  $dbh->exec("CREATE DATABASE IF NOT EXISTS `site`;")  or die(print_r($dbh->errorInfo(), true));
+
+  // Course-admins id INT, can_create INT, can_archive INT, can_modify_permissions INT, can_create_roles INT
+  $dbh->exec("CREATE TABLE IF NOT EXISTS `site`.`users` (
+      `id`                    INT       NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing id',
+      PRIMARY KEY (`id`)
+   ) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='';") or die(print_r($dbh->errorInfo(), true));
 
   // Create database login
   $dbh->exec("CREATE DATABASE IF NOT EXISTS `login`;") or die(print_r($dbh->errorInfo(), true));
