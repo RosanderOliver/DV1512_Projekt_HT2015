0cc5ad30bea6e209cd539305e50f5223491d6af6
Jim Ahlstrand <jiah13@student.bth.se>
2015-11-22 14:31:55 +0100

    Fixed path issues with tests

--- tests/LoginTest.php
+++ tests/LoginTest.php
@@ -1,11 +1,13 @@
 <?php
 
+  ob_start();
+
   class LoginTest extends PHPUnit_Framework_TestCase
   {
     public function testConstruct()
     {
       $a = new Login();
       $this->assertNotNull($a);
-      $this->assertEquals(Array(), $a->errors)
+      $this->assertEquals(Array(), $a->errors);
     }
   }
--- tests/testAutoloader.php
+++ tests/testAutoloader.php
@@ -3,19 +3,21 @@
   define('IN_EXM', TRUE);
 
   // We need the defenitions
-  require_once('/www/includes/config.php');
+  require_once('www/includes/config.php');
 
   if (version_compare(phpversion(), '5.1.2', '<')) {
     die('Autoloader requires PHP > 5.1.2');
   }
   else if (version_compare(phpversion(), '5.3.0', '<')) {
     function classloader($class){
-      include PATH_CLASS . $class . '.php';
+      if(!class_exists($class, false))
+        include PATH_CLASS . $class . '.php';
     }
     spl_autoload_register('classloader');
   }
   else {
     spl_autoload_register(function ($class){
-      include '/www/' . PATH_CLASS . $class . '.php';
+      if(!class_exists($class, false))
+        include 'www/' . PATH_CLASS . $class . '.php';
     });
   }
