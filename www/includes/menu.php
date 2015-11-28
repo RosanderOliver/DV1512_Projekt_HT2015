5ec58fdb171f99f8f8f46dbb4a1dade187115ef7
Jim Ahlstrand <jiah13@student.bth.se>
2015-11-28 14:41:30 +0100

    Added function to print menues and lists

--- www/includes/menu.php
+++ www/includes/menu.php
@@ -3,4 +3,13 @@
   // Don't access this script alone
   if (!defined('IN_EXM')) exit;
 
-?>
+  $navigation = [
+    [ "Overview", "?view=overview" ],
+    [ "DEBUG", [
+        [ "Edit", "?view=edit" ],
+        [ "Examinator", "?view=examinator_page" ]
+    ]],
+    [ "Logout", "?logout" ]
+  ];
+
+  printMenu($navigation);
