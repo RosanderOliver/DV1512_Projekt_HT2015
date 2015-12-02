57c73c8bb4aa9d1c8892633b19c5f2dc2822ad9f
Jim Ahlstrand <jiah13@student.bth.se>
2015-12-02 10:03:33 +0100

    Fixed #31

--- www/includes/java/Jquery.js
+++ www/includes/java/Jquery.js
@@ -1,13 +1,21 @@
 
+var pageSize = function() {
+    $("#page").height($(document).height() - 30);
+}
+
 $(function() {
 
-    // Menu
-    $("#menu ul li ul").addClass( "submenu" );
-    $('.submenu li').hide();
-    $('.submenu').parent().hover(function () {
-      $(this).find("li").stop().show("slow");
-    }, function () {
-      $(this).find("li").stop().hide("slow");
-    });
+  // Page
+  pageSize();
+  $(window).resize(pageSize());
+
+  // Menu
+  $("#menu ul li ul").addClass( "submenu" );
+  $(".submenu li").hide();
+  $(".submenu").parent().hover(function () {
+    $(this).find("li").stop().show("slow");
+  }, function () {
+    $(this).find("li").stop().hide("slow");
+  });
 
 });
