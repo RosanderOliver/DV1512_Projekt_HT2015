099497879418e1c052d45a9dc469f3f3e728e3ae
Annika <akinna_nossnah@hotmail.com>
2015-12-08 12:04:14 +0100

    SQL calls defined

--- www/includes/SQL.php
+++ www/includes/SQL.php
@@ -9,3 +9,7 @@
 define("SQL_SELECT_USER_WHERE_ID", "SELECT * FROM `site`.`users` WHERE `id`= :id");
 define("SQL_SELECT_USER_WHERE_EPPN", "SELECT * FROM `site`.`users` WHERE `eppn`= :eppn");
 define("SQL_INSERT_USER", "INSERT INTO `site`.`users` (`eppn`, `user_email`) VALUES (:eppn, :user_email)");
+
+//reviewer
+define("SQL_SELECT_PROJECTS_BY_REVIEWER","SELECT * FROM `site`.`projects` WHERE `reviewers`=:reviewers");
+define("SQL_INSERT_USER_AS_FEASIBLE_REVIEWERS","INSERT INTO `site`.`projects` WHERE `feasible_reviewers`=:feasible_reviewers");
