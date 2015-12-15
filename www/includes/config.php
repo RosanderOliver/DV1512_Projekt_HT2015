b6c35cdec831760a605fdaaa93687e248cac51b2
Jim Ahlstrand <jiah13@student.bth.se>
2015-12-15 18:12:34 +0100

    Fixed comments for reviews, minor issues with code standard also fixed, some styling for reviews

--- www/includes/config.php
+++ www/includes/config.php
@@ -123,9 +123,11 @@ define("HASH_COST_FACTOR", "12");
   * Configuration for: maximum lengths
   * Defines maximum lengths of data variables, this is limited by the database
   *
-  * MAX_COMMENT_LENGTH: length of comments data
+  * MAX_COMMENT_LENGTH: length of contents of comments
+  * MAX_COMMENT_DEPTH: maximum printed depth of comments and responses to comments
   */
   define("MAX_COMMENT_LENGTH", 256);
+  define("MAX_COMMENT_DEPTH", 3);
 
   /**
    * Configuration for: project stages
